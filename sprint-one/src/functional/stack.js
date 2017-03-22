0dc59a0002d8f3769cbba3b3a8dd0370b7678725
MKS-Student <mks.student@hackreactor.com>
2017-03-22 15:23:40 -0700

    Instantiates the function pattern of stack with tests passing.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,25 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var topKey = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[topKey] = value;
+    topKey++;
   };
 
   someInstance.pop = function() {
+    if (topKey > 0) {
+      var top = storage[topKey - 1];
+      delete storage[topKey - 1];
+      topKey--;
+      return top;
+    }
   };
 
   someInstance.size = function() {
+    return topKey;
   };
 
   return someInstance;
