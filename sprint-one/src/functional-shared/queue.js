9914931595b829c6563e18f469a416997fa4e7fb
MKS-Student <mks.student@hackreactor.com>
2017-03-22 16:39:25 -0700

    Instantiates a queue in the functional-shared pattern with all tests passing.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,29 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var queueInstance = {};
+  queueInstance.storage = {};
+
+  _.extend(queueInstance, queueMethods);
+
+  return queueInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function (value) {
+    this.storage[Object.keys(this.storage).length] = value;
+  },
+  dequeue: function () {
+    var output = this.storage[0];
+
+    for (var i = 0; i < Object.keys(this.storage).length - 1; i++) {
+      this.storage[i] = this.storage[i + 1];
+    }
+    delete this.storage[Object.keys(this.storage).length - 1];
+
+    return output;
+  },
+  size: function () { 
+    return Object.keys(this.storage).length;
+  }
+};
 
 
