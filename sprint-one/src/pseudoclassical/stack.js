ef7d482c8b8688228a65e34ff328ca0e168e3721
MKS-Student <mks.student@hackreactor.com>
2017-03-22 15:56:11 -0700

    intantiates stacks in the pseudoclassical pattern

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.topKey = 0;
+  this.storage = {};
 };
 
+Stack.prototype.size = function() {
+  return this.topKey;
+}
 
+Stack.prototype.push = function(value) {
+  this.storage[this.topKey] = value;
+  this.topKey++;
+}
+
+Stack.prototype.pop = function() {
+  if (this.topKey > 0) {
+    var top = this.storage[this.topKey - 1];
+    delete this.storage[this.topKey - 1];
+    this.topKey--;
+    return top;
+  }
+}
