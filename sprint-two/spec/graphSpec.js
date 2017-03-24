53fafb28acd18c53dc315a0e7c355938c372b2a0
MKS-Student <mks.student@hackreactor.com>
2017-03-24 16:23:36 -0700

    Now has test to ensure that the nodes are NOT stored in an Array.

--- sprint-two/spec/graphSpec.js
+++ sprint-two/spec/graphSpec.js
@@ -36,6 +36,14 @@ describe('graph', function() {
     expect(graph.hasEdge(3, 1)).to.equal(false);
   });
 
+  it('should not store edges in an Array', function () {
+    graph.addNode(2);
+    graph.addNode(1);
+    graph.addNode(3);
+    graph.addEdge(3, 2);
+    expect(Array.isArray(graph.nodes[2].edges)).to.equal(false);
+  });
+
   it('should remove edges between nodes', function() {
     graph.addNode(4);
     graph.addNode(5);
