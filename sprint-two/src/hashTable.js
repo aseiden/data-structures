d298e6a7831288158e37670cac1b8e55fed1953b
MKS-Student <mks.student@hackreactor.com>
2017-03-24 11:20:26 -0700

    Implemented hashTable mimimum requirements with passing tests.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,18 +3,43 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+
+  for (var i = 0; i < this._limit; i++) {
+    this._storage.set(i, []);
+  }
+
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+
+  bucket.push([k, v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  var output;
+  bucket.forEach(function (tuple) {
+    if (tuple[0] === k) {
+      output = tuple[1];
+    }
+  });
+  return output;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  var tupleIndex;
+
+  bucket.forEach(function (tuple, i) {
+    if (tuple[0] === k) {
+      tupleIndex = i;
+    }
+  });
+  bucket.splice(tupleIndex, 1);
 };
 
 
